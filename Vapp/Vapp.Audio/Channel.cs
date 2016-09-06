e32484f031bb1f0d9b7400ccf8831f686dabc302
Jagreaper <jamesalexgrant@hotmail.com>
2016-09-06 17:11:00 +1000

    Added basic audio format

---
+++ Vapp/Vapp.Audio/Channel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vapp.Audio
+{
+    public class Channel
+    {
+        public Channel(int id)
+        {
+            this.ID = id;
+        }
+
+        public int ID { get; }
+
+        public List<Sample> Samples { get; set; } = new List<Sample>();
+    }
+}
---
+++ Vapp/Vapp.Audio/Sound.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vapp.Audio
+{
+    public class Sound
+    {
+        #region Properties
+
+        public Channel[] Channels { get; set; }
+
+        public short AudioFormat { get; set; }
+
+        public int SampleRate { get; set; }
+
+        public int ByteRate { get; set; }
+
+        public short BlockAlign { get; set; }
+
+        public short BitsPerSample { get; set; }
+
+        #endregion
+    }
+}
