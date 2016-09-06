02d4c0248fa88ffa3a521b577a1f88ef2c84021d
Jagreaper <jamesalexgrant@hotmail.com>
2016-09-06 17:11:13 +1000

    added basic extensions

---
+++ Vapp/Vapp.Extensions/ObservableCollectionExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vapp.Extensions
+{
+    public static class ObservableCollectionExtension
+    {
+        public static void AddRange<T>(this ObservableCollection<T> host, IEnumerable<T> items)
+        {
+            foreach (T item in items)
+                host.Add(item);
+        }
+    }
+}
---
+++ Vapp/Vapp.Extensions/StreamExtension.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Vapp.Extensions
+{
+    public static class StreamExtension
+    {
+        public static void WriteInt(this Stream host, int value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            host.Write(bytes, 0, 4);
+        }
+
+        public static int ReadInt(this Stream host)
+        {
+            byte[] bytes = new byte[4];
+            host.Read(bytes, 0, 4);
+            return BitConverter.ToInt32(bytes, 0);
+        }
+
+        public static void WriteShort(this Stream host, short value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            host.Write(bytes, 0, 2);
+        }
+
+        public static short ReadShort(this Stream host)
+        {
+            byte[] bytes = new byte[2];
+            host.Read(bytes, 0, 2);
+            return BitConverter.ToInt16(bytes, 0);
+        }
+    }
+}
