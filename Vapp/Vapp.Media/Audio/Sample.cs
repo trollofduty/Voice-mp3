cd6d5f05d34438523a3d3c2a52d9f9f0001c9354
Jagreaper <jamesalexgrant@hotmail.com>
2016-09-18 21:00:55 +1000

    Added Sample Set and Get value support for all formats except 24bit int

--- Vapp/Vapp.Media/Audio/Sample.cs
+++ Vapp/Vapp.Media/Audio/Sample.cs
@@ -21,10 +21,60 @@ namespace Vapp.Media.Audio
         #region Properties
 
         public byte[] Data { get; private set; }
+        
+        public double Value
+        {
+            get { return this.GetValue(); }
+            set { this.SetValue(value); }
+        }
 
         #endregion
 
         #region Methods
+
+        private void SetValue(double value)
+        {
+            switch (this.Data.Length)
+            {
+                default:
+                    throw new FormatException();
+                case 1:
+                    this.Data[0] = (byte) (((value + 1.0) / 2.0) * 0xF);
+                    break;
+                case 2:
+                    short sValue = (short) (value >= 0 ? value * short.MaxValue : value * short.MinValue);
+                    this.Data = BitConverter.GetBytes(sValue);
+                    break;
+                case 4:
+                    int iValue = (int) (value >= 0 ? value * int.MaxValue : value * int.MinValue);
+                    this.Data = BitConverter.GetBytes(iValue);
+                    break;
+                case 8:
+                    int lValue = (int) (value >= 0 ? value * long.MaxValue : value * long.MinValue);
+                    this.Data = BitConverter.GetBytes(lValue);
+                    break;
+            }
+        }
+
+        private double GetValue()
+        {
+            switch (this.Data.Length)
+            {
+                default:
+                    throw new FormatException();
+                case 1:
+                    return ((this.Data[0] / (double) 0xF) * 2.0) - 1.0;
+                case 2:
+                    short sValue = BitConverter.ToInt16(this.Data, 0);
+                    return sValue >= 0 ? sValue / (double) short.MaxValue : sValue / (double) short.MinValue;
+                case 4:
+                    int iValue = BitConverter.ToInt32(this.Data, 0);
+                    return iValue >= 0 ? iValue / (double) int.MaxValue : iValue / (double) int.MinValue;
+                case 8:
+                    long lValue = BitConverter.ToInt64(this.Data, 0);
+                    return lValue >= 0 ? lValue / (double) long.MaxValue : lValue / (double) long.MinValue;
+            }
+        }
 
         public Sample DeepClone()
         {
