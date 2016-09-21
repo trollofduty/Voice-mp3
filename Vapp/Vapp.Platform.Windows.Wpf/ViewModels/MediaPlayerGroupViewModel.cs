e9e24543759af340795940b5f764db3a7be50123
Jagreaper <jamesalexgrant@hotmail.com>
2016-09-21 16:09:04 +1000

    Added Storyboard fade out option when media player is in fullscreen mode

--- Vapp/Vapp.Platform.Windows.Wpf/ViewModels/MediaPlayerGroupViewModel.cs
+++ Vapp/Vapp.Platform.Windows.Wpf/ViewModels/MediaPlayerGroupViewModel.cs
@@ -18,11 +18,34 @@ namespace Vapp.Platform.Windows.Wpf.ViewModels
             MediaPlayerView mediaPlayerView = new MediaPlayerView();
             this.MediaOutput = mediaPlayerView;
             this.MediaControls = new MediaPlayerControlsView(ref mediaPlayerView);
+            this.IsFullscreen = true;
         }
 
         #endregion
 
         #region Properties
+
+        private bool isFullscreen;
+        public bool IsFullscreen
+        {
+            get { return this.isFullscreen; }
+            set
+            {
+                this.Set(ref this.isFullscreen, value);
+
+                if (this.isFullscreen)
+                    this.RowSpan = 2;
+                else
+                    this.RowSpan = 1;
+            }
+        }
+
+        private int rowSpan;
+        public int RowSpan
+        {
+            get { return this.rowSpan; }
+            set { this.Set(ref rowSpan, value); }
+        }
 
         private UserControl mediaOutput;
         public UserControl MediaOutput
