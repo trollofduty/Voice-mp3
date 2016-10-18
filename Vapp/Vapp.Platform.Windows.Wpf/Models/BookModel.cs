6c2320be1015101571a80a9e3c817b4b4a5957d7
Jagreaper <jamesalexgrant@hotmail.com>
2016-10-18 15:31:54 +1000

    Added ImportBibleTextSubView

---
+++ Vapp/Vapp.Platform.Windows.Wpf/Models/BookModel.cs
@@ -0,0 +1,80 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vapp.IO.Codecs.Text;
+
+namespace Vapp.Platform.Windows.Wpf.Models
+{
+    class BookModel : ObservableObject
+    {
+        #region Constructor
+
+        public BookModel(Book book)
+        {
+            this.Book = book;
+        }
+
+        #endregion
+
+        #region Properties
+
+        private Book book;
+        public Book Book
+        {
+            get { return this.book; }
+            set
+            {
+                this.Set(ref this.book, value);
+                this.RaisePropertyChanged("Order");
+                this.RaisePropertyChanged("Name");
+                this.RaisePropertyChanged("ChaptersCount");
+                this.RaisePropertyChanged("VersesCount");
+            }
+        }
+
+        public int Order
+        {
+            get { return this.Book.Order; }
+            set
+            {
+                this.Book.Order = value;
+                this.RaisePropertyChanged("Order");
+            }
+        }
+
+        public string Name
+        {
+            get { return this.Book.Name; }
+            set
+            {
+                this.Book.Name = value;
+                this.RaisePropertyChanged("Name");
+            }
+        }
+
+        public int ChaptersCount
+        {
+            get
+            {
+                int count = 0;
+                foreach (VerseToken token in this.Book.VerseList.Select(t => t.Key))
+                    count = token.ChapterNumber > count ? token.ChapterNumber : count;
+
+                return count;
+            }
+        }
+
+        public int VersesCount
+        {
+            get
+            {
+                return this.Book.VerseList.Count;
+            }
+        }
+
+        #endregion
+    }
+}
--- Vapp/Vapp.Platform.Windows.Wpf/ViewModels/Wizard/ImportBibleTextSubViewModel.cs
+++ Vapp/Vapp.Platform.Windows.Wpf/ViewModels/Wizard/ImportBibleTextSubViewModel.cs
@@ -1,19 +1,89 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+using Vapp.IO.Codecs.Text;
+using Vapp.Platform.Windows.Wpf.Models;
+using Vapp.Extensions;
 
 namespace Vapp.Platform.Windows.Wpf.ViewModels.Wizard
 {
     class ImportBibleTextSubViewModel : ViewModelBase, IWizardResult
     {
+        #region Constructor
+
+        public ImportBibleTextSubViewModel()
+        {
+            this.OpenFileCommand = new RelayCommand(this.OpenFile);
+            this.RemoveCommand = new RelayCommand(this.RemoveBook);
+        }
+        
+        #endregion
+
         #region Properties
+
+        public ObservableCollection<BookModel> BookList { get; private set; } = new ObservableCollection<BookModel>();
+
+        public ICommand OpenFileCommand { get; set; }
+
+        public ICommand RemoveCommand { get; set; }
+
+        public BookModel selectedBook;
+        public BookModel SelectedBook
+        {
+            get { return this.selectedBook; }
+            set { this.Set(ref this.selectedBook, value); }
+        }
+
+        private BibleDecoder decoder = new BibleDecoder();
+
+        private Bible bible;
 
         #endregion
 
         #region Methods
+
+        public void OpenFile()
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Filter = @"All Files(*.*) | *.*";
+                dlg.Multiselect = false;
+                DialogResult result = dlg.ShowDialog();
+
+                if (result == DialogResult.OK)
+                {
+                    App.Current.Dispatcher.Invoke(this.BookList.Clear);
+                    Stream stream = File.OpenRead(dlg.FileName);
+
+                    if (this.decoder.TryDecode(stream, out this.bible))
+                    {
+                        App.Current.Dispatcher.Invoke(() => this.BookList.AddRange(this.bible.BookList.Select(t => new BookModel(t.Value))));
+                    }
+                }
+            }
+
+            GC.Collect();
+            GC.Collect();
+        }
+
+        public void RemoveBook()
+        {
+            if (this.BookList.Contains(this.SelectedBook))
+            {
+                App.Current.Dispatcher.Invoke(() =>
+                {
+                    this.BookList.Remove(this.SelectedBook);
+                });
+            }
+        }
 
         public bool Execute()
         {
