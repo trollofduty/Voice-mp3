0f85c82c761b35fa0b0d764ca156345b38681304
Jagreaper <jamesalexgrant@hotmail.com>
2016-10-27 02:24:34 +1000

    Added in xaml debug condition for debug open menu item

--- Vapp/Vapp.Platform.Windows.Wpf/Properties/AssemblyInfo.cs
+++ Vapp/Vapp.Platform.Windows.Wpf/Properties/AssemblyInfo.cs
@@ -3,6 +3,7 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
+using System.Windows.Markup;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -40,6 +41,9 @@ using System.Windows;
                                               // app, or any theme specific resource dictionaries)
 )]
 
+#if DEBUG
+[assembly: XmlnsDefinition("debug-mode", "Namespace")]
+#endif
 
 // Version information for an assembly consists of the following four values:
 //
